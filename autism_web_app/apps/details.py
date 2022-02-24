9d3f2cfe1f6b7f2241b8a27a369c0b054e6fb755
Kazeem Hakeem <hakymulla@gmail.com>
2022-02-24 15:37:40 +0100

    Get cwd1

--- autism_web_app/apps/details.py
+++ autism_web_app/apps/details.py
@@ -16,7 +16,8 @@ def app():
     cwd = os.getcwd()  # Get the current working directory (cwd)
     files = os.listdir(cwd)  # Get all the files in that directory
     print("Files in %r: %s" % (cwd, files))
-
+    st.write(cwd)
+    st.write(files)
     st.markdown("""
     # Autism Prediction App
 
